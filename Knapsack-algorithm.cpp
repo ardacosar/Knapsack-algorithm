acc10db9ecd09e1060e7be9b50472d33c142900a
Arda <ardacosarr@gmail.com>
2020-06-16 23:54:21 +0300

    v1.1

--- Knapsack-algorithm.cpp
+++ Knapsack-algorithm.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 struct Item
 {
 	string name;
@@ -41,45 +42,48 @@ void define(Item* it)
 	it[6].weight = 9;
 	it[6].value = 100;
 }
-void solve(Item* it)
+void solve(Item* it , int knapWeight)
 {
-	Item Ss[127]; //Subset array | 127 = 2^7-1
+	Item subSet[127]; //Subset array | 127 = 2^7-1
 	int maxValue = 0, trueIndex; //trueIndex is Index that holds the maxValue in Subsets;
 
 	for (int i = 0; i < 7; i++) // First members
-	{ Ss[i] = it[i]; }	
+	{ subSet[i] = it[i]; }	
 	
 	int s = 7; // Counter | 0 to 127
 
 	for (int i = 0; i < 120; i++)  // i < (Subset Array's length)-(Item Array's length) | i < 127-7
 	{
 
-		for (int j = Ss[i].index + 1; j < 7; j++)   //Combination Phase
+		for (int j = subSet[i].index + 1; j < 7; j++)   //Combination Phase
 		{
-			Ss[s].name = Ss[i].name + it[j].name;
-			Ss[s].value = Ss[i].value + it[j].value;
-			Ss[s].weight = Ss[i].weight + it[j].weight;
-			Ss[s].index = it[j].index;
+			subSet[s].name = subSet[i].name + it[j].name;
+			subSet[s].value = subSet[i].value + it[j].value;
+			subSet[s].weight = subSet[i].weight + it[j].weight;
+			subSet[s].index = it[j].index;
 
-			if (Ss[s].value > maxValue && Ss[s].weight <= 25)  
+			if (subSet[s].value > maxValue && subSet[s].weight <= knapWeight)  
 			{
-				maxValue = Ss[s].value;
+				maxValue = subSet[s].value;
 				trueIndex = s;
 			}
 
 			s++;
 		}
 	}
-	cout << "Items: " << Ss[trueIndex].name << " - " << "Weight: " << Ss[trueIndex].weight << "Kg"  << " - " << "Value: " << Ss[trueIndex].value << "$" << endl;  
+	cout << "Items: " << subSet[trueIndex].name << " - " << "Weight: " << subSet[trueIndex].weight << "Kg"  << " - " << "Value: " << subSet[trueIndex].value << "$" << endl;  
 }
 int main()
 {
-
+	int knapsackWeight;
+	cout << "Knapsack Weight: ";
+	cin >> knapsackWeight;
+	
 	Item it[7];
 
 	define(it);
 
-	solve(it);
+	solve(it, knapsackWeight);
 
 
 }
